0f23a1cb46210a96961453fd44631f5302d36cec
Nathan Rajlich <nathan@tootallnate.net>
2012-12-16 11:32:30 -0800

    receiver: better UDP "message" event log message

--- receiver.js
+++ receiver.js
@@ -54,7 +54,7 @@ Object.keys(PORTS).forEach(function (name) {
       port: port,
       msg: msg.toString('binary')
     };
-    console.log('"message" port %d (%d byes)', obj.port, msg.length);
+    console.log('"message" on port %d from %s:%s (%d bytes)', obj.port, rinfo.address, rinfo.port, msg.length);
     io.emit('udp', obj);
   });
 
