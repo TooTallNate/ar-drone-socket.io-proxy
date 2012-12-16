65705b34a97ee199c8b18a473bd853d069f74758
Nathan Rajlich <nathan@tootallnate.net>
2012-12-15 18:27:13 -0800

    ports: specify the TCP/UDP ports

--- ports.js
+++ ports.js
@@ -5,13 +5,13 @@
  */
 
 module.exports = {
-  FTP: 5551,
-  AUTH: 5552,
-  VIDEO_RECORDER: 5553,
-  NAVDATA: 5554,
-  VIDEO: 5555,
-  AT: 5556,
-  RAW_CAPTURE: 5557,
-  PRINTF: 5558,
-  CONTROL: 5559
+  FTP: 5551,            // tcp
+  AUTH: 5552,           // ???
+  VIDEO_RECORDER: 5553, // tcp
+  NAVDATA: 5554,        // udp
+  VIDEO: 5555,          // tcp
+  AT: 5556,             // udp
+  RAW_CAPTURE: 5557,    // tcp
+  PRINTF: 5558,         // ???
+  CONTROL: 5559         // tcp
 };
